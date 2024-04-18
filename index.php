b6abb1e89a0963212b040f26e65cd05478972a5a
hw-lyu <lyuhye03@gmail.com>
2024-04-18 18:04:54 +0900

    feat : 레디스 관련 Psr16Cache로 세팅

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 declare(strict_types=1);
 require_once 'vendor/autoload.php';
-//require 'vendor/predis/predis/autoload.php';
 
-use App\Redis;
+use Symfony\Component\Cache\Adapter\RedisAdapter;
+use Symfony\Component\Cache\Psr16Cache;
+use Psr\SimpleCache\InvalidArgumentException;
 
-$redis = new Redis();
+$redis = new \Redis();
+try {
+    $redis->connect(host: 'redis', port: 13000);
+
+    $adapter = new RedisAdapter($redis);
+    $redisCache = new Psr16Cache($adapter);
+
+    $redisCache->set(key: 'a', value: 1);
+    $redisCache->setMultiple(['b' => '', 'c' => '']);
+
+    print_r($redisCache->get('a'));
+    print_r($redisCache->getMultiple(['b', 'c']));
+
+} catch (InvalidArgumentException $e) {
+    return $e->getMessage();
+}
+
 
